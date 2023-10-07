cef6e324fd0f8a159363f20f43570d6ebfa5fe55
Adit Khandelwal <khandelwal.adit@gmail.com>
2023-10-06 20:51:54 -0700

    Setting up FastTrack Market Data API

--- stockAI.py
+++ stockAI.py
@@ -1,4 +1,23 @@
-print("Hello World")
-print("H")
-print("Test Commit - Adit")
-# Hello
+import requests
+import json
+
+# find way to hide secret.txt
+file = open("tokens.txt")
+content = file.readlines()
+
+
+# use your own account number, password and appid to authenticate (assigned from tokens.txt)
+account = str(content[0])
+password = content[1]
+appid = content[2]
+
+# Build auth url
+authurl = "https://ftl.fasttrack.net/v1/auth/login?account=" + account + "&pass=" + password + "&appid=" + appid 
+
+# make authentication request
+authresponse = requests.request("GET", authurl)
+
+# parse result and extract token
+a = json.loads(authresponse.text)
+
+token = a['token']
