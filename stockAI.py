67c796ea5e42d4fdbcc4a9fd279e158c4fe6d4c0
Vishal <venkateswaranvishal@gmail.com>
2023-10-07 00:04:22 -0700

    dataDays() created and API figured out

--- stockAI.py
+++ stockAI.py
@@ -1,5 +1,12 @@
 import requests
 import json
+from sklearn.ensemble import RandomForestRegressor
+from datetime import datetime, timedelta
+import pandas as pd
+import numpy as np 
+import yfinance as yf
+
+
 
 # find way to hide secret.txt
 file = open("tokens.txt")
@@ -7,20 +14,46 @@ content = file.readlines()
 
 
 # use your own account number, password and appid to authenticate (assigned from tokens.txt)
-account = str(content[0]).strip()
-password = content[1].strip()
-appid = content[2].strip()
+APIKEY = str(content[0]).strip()
+TICKER = input("Enter a ticker: ")
+url = "https://api.polygon.io/v1/open-close/AAPL/2023-09-17?adjusted=true&apiKey=" + APIKEY
 
-# Build auth url
-authurl = "https://ftl.fasttrack.net/v1/auth/login?account=" + account + "&pass=" + password + "&appid=" + appid 
+# response = requests.get(url)
 
-# make authentication request
-authresponse = requests.request("GET", authurl)
+# json_data = response.json()
 
-# parse result and extract token
-a = json.loads(authresponse.text)
+# print(json_data)
 
-token = a['token']
+now = datetime.now().day
+
+def dataDays():
+    current_date = datetime.now()
+    for i in range(14):
+    # Calculate the date i days ago
+        temp = current_date - timedelta(days=i)
+        day = temp.strftime("%d")
+        month = temp.strftime("%m")
+        url = "https://api.polygon.io/v1/open-close/" + TICKER + "/2023-" + str(day) + "-" + str(month) + "?adjusted=true&apiKey=" + APIKEY
+        
+        #Print out JSON
+        response = requests.get(url)
+        json_data = response.json()
+        # if (json_data["status"] == "OK"):
+            # print(json_data)
+            # print("Date: " + str(json_data["from"]) + " | Open Price: " + str(json_data["open"]) + " | Close Price: " + str(json_data["close"]))
+
+dataDays()
+
+# url = "https://api.polygon.io/v1/open-close/" + TICKER + "/2023-0" + str(DATE) + "-17?adjusted=true&apiKey=" + APIKEY
+
+# response = requests.get(url)
+
+# json_data = response.json()
+
+# print(json_data)
+
+# print("Date: " + str(json_data["from"]) + " | Open Price: " + str(json_data["open"]) + " | Close Price: " + str(json_data["close"]))
 
 
-print(a)
+    
+
