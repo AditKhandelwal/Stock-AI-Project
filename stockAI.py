73f743f500b35b91a89b0fa7b3ba4d1c78d2165f
Adit Khandelwal <khandelwal.adit@gmail.com>
2023-10-07 15:03:50 -0700

    Full switch to Yahoo Finance and created function to get 14 days of data.

--- stockAI.py
+++ stockAI.py
@@ -6,20 +6,10 @@ import pandas as pd
 import numpy as np 
 import yfinance as yf
 from finta import TA
+import matplotlib.pyplot as plt
 
-# find way to hide tokens.txt
-# file = open("tokens.txt")
-# content = file.readlines()
-
-
-# url = "https://api.polygon.io/v1/open-close/AAPL/2023-09-17?adjusted=true&apiKey=" + APIKEY
-# response = requests.get(url)
-# json_data = response.json()
-# print(json_data)
-
-# use your own account number, password and appid to authenticate (assigned from tokens.txt)
-# APIKEY = str(content[0]).strip()
-
+# Asks the user to input a stock ticker and validates the ticker to make sure that there are 
+# no invalid tickers being inputted. Also forces ticker to uppercase.
 def userInputTicker():
     TICKER = input("Enter a ticker: ")
     if (len(TICKER) > 5 or len(TICKER) == 0):
@@ -29,25 +19,34 @@ def userInputTicker():
         TICKER = yf.Ticker(TICKER.upper())
         return TICKER
 
-# now = datetime.now().day
 
-# def dataDays(TICKER):
-#     current_date = datetime.now()
-#     for i in range(14):
-#     # Calculate the date i days ago
-#         temp = current_date - timedelta(days=i)
-#         day = temp.strftime("%d")
-#         month = temp.strftime("%m")
-#         url = "https://api.polygon.io/v1/open-close/" + TICKER + "/2023-" + str(day) + "-" + str(month) + "?adjusted=true&apiKey=" + APIKEY
+# Get date for 14 datalines
+def getPastDate():
+    weekdayCount = 0
+    weekendCount = 0
+    current_date = datetime.now()
+
+    past_date = current_date - timedelta(days=weekendCount)
+
+
+    while(weekdayCount != 14):
+        if past_date.weekday() < 5:  # Saturday (5) or Sunday (6)
+            weekdayCount += 1
+        weekendCount += 1
+        past_date = current_date - timedelta(days=weekendCount)
         
-#         #Print out JSON
-#         response = requests.get(url)
-#         json_data = response.json()
-#         if (json_data["status"] == "OK"):
-#             #print(json_data)
-#             print("Date: " + str(json_data["from"]) + " | Open Price: " + str(json_data["open"]) + " | Close Price: " + str(json_data["close"]))
+    return past_date
 
-#print(userInputTicker())
-#dataDays(str(userInputTicker()))
-#dataDays("TSLA")
-#dataDays("tsla")
+print(getPastDate())
+
+
+
+# Download data for TICKER
+#tickerData = yf.download(str(userInputTicker()), start="20" + pastYear + "-" + pastMonth +  "-" + pastDay, end="20" + currYear + "-" + currMonth +  "-" + currDay)
+
+# Print the first 5 rows of the data
+# print(len(testData))
+# print(testData.head(4))
+
+#print(tickerData.loc[1])
+# print(tickerData.iloc[3, 4])
