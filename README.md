4ed13ec8ac46d7798637bd9b0c9d6322941c9dbc
Vishal <venkateswaranvishal@gmail.com>
2023-10-07 14:10:27 -0700

    Switching to Yahoo Finance API and modified the ReadMe file

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Stock-AI-Project
-stock API project to predict stock prices
+---
+## This is a python project to use a machine learning model to predict the price of a stock based on technical indicators.
+> *Packages will be listed here and updated as the project progresses:*
+- Requests
+- JSON
+- Scikit-Learn
+- Datetime
+- Pandas
+- NumPy
+- YFinance
+- Finta
+
+---
+
+### Authors:
+> **Adit Khandelwal and Vishal Venkateswaran**
--- stockAI.py
+++ stockAI.py
@@ -7,11 +7,9 @@ import numpy as np
 import yfinance as yf
 from finta import TA
 
-
-
 # find way to hide tokens.txt
-file = open("tokens.txt")
-content = file.readlines()
+# file = open("tokens.txt")
+# content = file.readlines()
 
 
 # url = "https://api.polygon.io/v1/open-close/AAPL/2023-09-17?adjusted=true&apiKey=" + APIKEY
@@ -20,7 +18,7 @@ content = file.readlines()
 # print(json_data)
 
 # use your own account number, password and appid to authenticate (assigned from tokens.txt)
-APIKEY = str(content[0]).strip()
+# APIKEY = str(content[0]).strip()
 
 def userInputTicker():
     TICKER = input("Enter a ticker: ")
@@ -28,30 +26,28 @@ def userInputTicker():
         print("Stock symbols don't typically have more than 5 letters and definitely don't have 0 letters. Enter a valid ticker: ")
         userInputTicker()
     else:
-        TICKER = TICKER.upper()
+        TICKER = yf.Ticker(TICKER.upper())
         return TICKER
 
+# now = datetime.now().day
 
-
-now = datetime.now().day
-
-def dataDays(TICKER):
-    current_date = datetime.now()
-    for i in range(14):
-    # Calculate the date i days ago
-        temp = current_date - timedelta(days=i)
-        day = temp.strftime("%d")
-        month = temp.strftime("%m")
-        url = "https://api.polygon.io/v1/open-close/" + TICKER + "/2023-" + str(day) + "-" + str(month) + "?adjusted=true&apiKey=" + APIKEY
+# def dataDays(TICKER):
+#     current_date = datetime.now()
+#     for i in range(14):
+#     # Calculate the date i days ago
+#         temp = current_date - timedelta(days=i)
+#         day = temp.strftime("%d")
+#         month = temp.strftime("%m")
+#         url = "https://api.polygon.io/v1/open-close/" + TICKER + "/2023-" + str(day) + "-" + str(month) + "?adjusted=true&apiKey=" + APIKEY
         
-        #Print out JSON
-        response = requests.get(url)
-        json_data = response.json()
-        if (json_data["status"] == "OK"):
-            #print(json_data)
-            print("Date: " + str(json_data["from"]) + " | Open Price: " + str(json_data["open"]) + " | Close Price: " + str(json_data["close"]))
+#         #Print out JSON
+#         response = requests.get(url)
+#         json_data = response.json()
+#         if (json_data["status"] == "OK"):
+#             #print(json_data)
+#             print("Date: " + str(json_data["from"]) + " | Open Price: " + str(json_data["open"]) + " | Close Price: " + str(json_data["close"]))
 
 #print(userInputTicker())
-dataDays(str(userInputTicker()))
+#dataDays(str(userInputTicker()))
 #dataDays("TSLA")
 #dataDays("tsla")
