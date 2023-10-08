9641f8d4594522a939148f3a9897c1abc92e3192
Adit Khandelwal <khandelwal.adit@gmail.com>
2023-10-07 17:03:34 -0700

    Created checkValidTicker and getTickerData function tto grab data for specified stock for the past 14 days

--- stockAI.py
+++ stockAI.py
@@ -4,81 +4,81 @@ from sklearn.ensemble import RandomForestClassifier
 from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np 
-import yfinance as yf
 from finta import TA
 import matplotlib.pyplot as plt
-
-indicators = ['RSI', 'MACD', 'STOCH', 'ADL', 'ATR', ]
+import yfinance as yf
 
 # Asks the user to input a stock ticker and validates the ticker to make sure that there are 
 # no invalid tickers being inputted. Also forces ticker to uppercase.
-def userInputTicker():
-    TICKER = str(input("Enter a ticker: ")).upper()
-    if (len(TICKER) > 5 or len(TICKER) == 0):
-        print("Stock symbols don't typically have more than 5 letters and definitely don't have 0 letters.")
-        userInputTicker()
-    else:
-        try:
-            stock_info = yf.Ticker(TICKER)
-            if stock_info.info:
-                print(stock_info.info)
-                return TICKER
-            else:
-                print(f"{TICKER} is not a valid stock ticker symbol.")
-                userInputTicker()
-        except ValueError:
-            print(f"{TICKER} is not a valid stock ticker symbol.")
-            userInputTicker()
 
-print(userInputTicker())
+
+def checkValidTicker():
+    valid = False
+    TICKER = str(input("Enter a ticker: ")).upper()
+    with open("nasdaq.txt", "r") as allowedTickersNASDAQ:
+        for line in allowedTickersNASDAQ:
+            firstItem = line.split("|")[0].strip()
+            if TICKER == firstItem.upper():
+                return TICKER
+
+    if not valid:
+        with open("nyse.txt", "r") as allowedTickersNYSE:
+            for line in allowedTickersNYSE:
+                firstItem = line.split(",")[0].strip()
+                if TICKER == firstItem.upper():
+                    return TICKER
+
+    # Prompt user again if invalid Ticker
+    if(valid == False):
+        print("Invalid Ticker. Please enter a valid ticker")
+        return checkValidTicker()
+    
+    allowedTickersNASDAQ.close()
+    allowedTickersNYSE.close()
+    
+    return TICKER
+
+
+
 #Smoothens out the data given from Yahoo Finance since having erratic data will cause an error in the Machine Learning model.
 def _exponential_smooth(data, alpha):
     return data.ewm(alpha = alpha).mean()
 
 # Get date for 14 datalines
-# def getPastDate():
-#     weekdayCount = 0
-#     weekendCount = 0
-#     current_date = datetime.now()
+def getPastDate():
+    weekdayCount = 0
+    weekendCount = 0
+    current_date = datetime.now()
 
-#     past_date = current_date - timedelta(days=weekendCount)
+    past_date = current_date - timedelta(days=weekendCount)
 
 
-#     while(weekdayCount != 14):
-#         if past_date.weekday() < 5:  # Saturday (5) or Sunday (6)
-#             weekdayCount += 1
-#         weekendCount += 1
-#         past_date = current_date - timedelta(days=weekendCount)
+    while(weekdayCount != 14):
+        if past_date.weekday() < 5:  # Saturday (5) or Sunday (6)
+            weekdayCount += 1
+        weekendCount += 1
+        past_date = current_date - timedelta(days=weekendCount)
 
-#     return past_date
+    return past_date
 
 # # create pandas object containing data for certain stock
-# def getTickerData():
-#     ticker = userInputTicker()
-#     currentDate = datetime.now()
-#     currDate = currentDate - timedelta(days=1)
-#     pastDate = getPastDate()
+def getTickerData():
+    ticker = str(checkValidTicker()).upper()
+    currentDate = datetime.now()
+    currDate = currentDate - timedelta(days=1)
+    pastDate = getPastDate()
 
-#     # Assign current/past date values
-#     currYear, currMonth, currDay = currDate.strftime("%y %m %d").split()
-#     pastYear, pastMonth, pastDay = pastDate.strftime("%y %m %d").split()
-#     tickerData = yf.download(ticker, start="20" + pastYear + "-" + pastMonth +  "-" + pastDay, end="20" + currYear + "-" + currMonth +  "-" + currDay)
-#     return tickerData
-
-
-# tickerData = getTickerData()
-# print(tickerData.head(14))
-
-# tickerDataSmooth = _exponential_smooth(tickerData, 0.65)
+    # Assign current/past date values
+    currYear, currMonth, currDay = currDate.strftime("%y %m %d").split()
+    pastYear, pastMonth, pastDay = pastDate.strftime("%y %m %d").split()
+    tickerData = yf.download(ticker, start="20" + pastYear + "-" + pastMonth +  "-" + pastDay, end="20" + currYear + "-" + currMonth +  "-" + currDay)
+    return tickerData
 
 
+tickerData = getTickerData()
+print(tickerData.head(3))
 
-# Download data for TICKER
-# tickerData = yf.download(str(userInputTicker()), start="20" + pastYear + "-" + pastMonth +  "-" + pastDay, end="20" + currYear + "-" + currMonth +  "-" + currDay)
-
-# Print the first 5 rows of the data
-# print(len(testData))
-# print(testData.head(4))
-
-#print(tickerData.loc[1])
+tickerDataSmooth = _exponential_smooth(tickerData, 0.65)
+print(tickerDataSmooth.head(3))
+# Get data by index location
 # print(tickerData.iloc[3, 4])
